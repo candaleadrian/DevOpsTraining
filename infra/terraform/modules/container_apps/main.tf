c86c76815f1bfc3f0ff7ebf4402f0c14592d1284
CANDALE, ADRIAN (PERO-SP) <adrian.candale@porsche-engineering.ro>
2026-03-23 16:57:02 +0200

    workload profile to frontend and backend

--- infra/terraform/modules/container_apps/main.tf
+++ infra/terraform/modules/container_apps/main.tf
@@ -20,6 +20,7 @@ resource "azurerm_container_app" "backend" {
   container_app_environment_id = azurerm_container_app_environment.main.id
   resource_group_name          = var.resource_group_name
   revision_mode                = "Single"
+  workload_profile_name        = "Consumption"
   tags                         = var.tags
 
   registry {
@@ -105,6 +106,7 @@ resource "azurerm_container_app" "frontend" {
   container_app_environment_id = azurerm_container_app_environment.main.id
   resource_group_name          = var.resource_group_name
   revision_mode                = "Single"
+  workload_profile_name        = "Consumption"
   tags                         = var.tags
 
   registry {
